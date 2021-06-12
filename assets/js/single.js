8c183d8a53d1df4e0d04a4f25ec5242265547e22
Aloha <utah.clayton@gmail.com>
2021-06-12 14:41:59 -0700

    redirect to home if response not valid

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,17 +1,25 @@
+
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
+    // grab repo from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
+    if(repoName) {
+        //display repo name on page
     repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to homepage
+        document.location.replace("./index.html");
+    }
 };
 
 var displayWarning = function(repo) {
     //add text to warning container
-    limitWarningEl.textContent = "To see more than 30 issues, visit";
+    limitWarningEl.textContent = "To see more than 30 issues, visit:";
     var linkEl = document.createElement("a");
     linkEl.textContent = "See More Issues on GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues"),
@@ -37,7 +45,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
